3839b83a42a454e8455725d88fe5cd204d579532
Zeljko Bajic <ZeljkoB@procom.ca>
2023-08-08 23:04:50 -0400

    Added new files, alert dialog and types.

---
+++ src/components/alert-dialog.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+
+const AlertDialog: React.FC = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const navigate = useNavigate();
+
+  const handleClose = () => {
+    setOpen(false);
+    navigate("/");
+  };
+
+  useEffect(
+    function openDialogOnMounting() {
+        setOpen(true);
+    }, []);
+
+  return (
+    <div>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Warning!"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Select an employee to complete the operation!
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} autoFocus>
+            OK
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
+
+export default AlertDialog;
---
+++ src/types/types.ts
@@ -0,0 +1,10 @@
+export enum FormMode {
+    ADD = "Add",
+    EDIT = "Edit",
+}
+ 
+export const initEmployee = {
+    id: 0,
+    firstName: "", lastName: "", email: "", phoneNumber: "",            
+    addresses: [{ streetName: "", apartmentNumber: 0, postalCode: "", state: "", country: "" }]
+};  
