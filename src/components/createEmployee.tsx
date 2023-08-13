bad83a4241264352d93c4a6d80ade525185f10b6
Zeljko Bajic <ZeljkoB@procom.ca>
2023-08-13 17:33:24 -0400

    Removed files from this path.

--- src/components/createEmployee.tsx
+++
@@ -1,8 +0,0 @@
-import { IFormComponentProps } from "../types/interfaces";
-
-const CreateEmployee: React.FC<IFormComponentProps> = (props) => {
-    const { formComponent } = props;
-    return <>{formComponent}</>;    
-};
-
-export default CreateEmployee;
--- src/components/editEmployee.tsx
+++
@@ -1,8 +0,0 @@
-import { IFormComponentProps } from "../types/interfaces";
-
-const EditEmployee: React.FC<IFormComponentProps> = (props) => {
-    const { formComponent } = props;
-    return <>{formComponent}</>;    
-};
-
-export default EditEmployee;
